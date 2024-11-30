d034f1e784a74fafb5c6b33b18566d2433cfd787
Suresh Kumar <ft24-bscs-0022@qu.edu.pk>
2024-11-30 22:46:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundColor = "red";
+let a = document.querySelector("h1");
+a.style.color="orange"; 
+a.style.backgroundColor ="black"; 
+a.style.border= "2px solid orange"; 
+a.style.borderRadius="5px"; 
